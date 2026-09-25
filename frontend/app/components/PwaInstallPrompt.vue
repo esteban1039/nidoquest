f82e727b131e5dc1004fefcb5308c580fd860bc1
Esteban Castaño <esteban1039@gmail.com>
2026-09-25 06:39:46 -0500

    Fix 500 en produccion: PwaInstallPrompt a la API nueva de usePwa

--- frontend/app/components/PwaInstallPrompt.vue
+++ frontend/app/components/PwaInstallPrompt.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 const { t } = useI18n()
-const { canInstall, install, enableNotifications, notificationPermission } = usePwa()
+const { canInstall, installApp, requestPushNotifications, pushPermission } = usePwa()
 const dismissed = useCookie<boolean>('nidoquest_pwa_prompt_dismissed', { sameSite: 'lax' })
 
-const show = computed(() => !dismissed.value && (canInstall.value || notificationPermission.value === 'default'))
+const show = computed(() => !dismissed.value && (canInstall.value || pushPermission.value === 'default'))
 </script>
 
 <template>
@@ -13,8 +13,8 @@ const show = computed(() => !dismissed.value && (canInstall.value || notificatio
       <p>{{ t('pwa.text') }}</p>
     </div>
     <div class="pwa-actions">
-      <button v-if="canInstall" class="button primary small" type="button" @click="install">{{ t('pwa.install') }}</button>
-      <button v-if="notificationPermission === 'default'" class="button ghost small" type="button" @click="enableNotifications">{{ t('pwa.notifications') }}</button>
+      <button v-if="canInstall" class="button primary small" type="button" @click="installApp">{{ t('pwa.install') }}</button>
+      <button v-if="pushPermission === 'default'" class="button ghost small" type="button" @click="requestPushNotifications">{{ t('pwa.notifications') }}</button>
       <button class="icon-button" type="button" :aria-label="t('pwa.dismiss')" @click="dismissed = true">×</button>
     </div>
   </aside>
